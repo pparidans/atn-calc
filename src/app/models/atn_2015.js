8818c5e8962fdd31961e42e6e8126802d6227e64
Pierre Paridans <pierre@paridans.com>
2014-12-18 09:54:59 +0100

    fix functions naming

--- src/app/models/atn_2015.js
+++ src/app/models/atn_2015.js
@@ -1,25 +1,26 @@
 var moment = require('moment');
 
 module.exports = {
+	minValue: 1250.00,
+
 	// TODO: extract yearly based values as Object properties
 	calculate: function(fuel_type, price, co2, buy_date, sell_date) {
 		// TODO: use buy_date & sell_date for prorata
 		// TODO: implement coefficientPrice
-		var atn = price * this.coefficientCo2(fuel_type, co2) * 6/7;
-		var minAtn = 1250;
-		if(atn < minAtn) {
-			return minAtn;
+		var atn = price * this.co2Coefficient(fuel_type, co2) * 6/7;
+		if(atn < this.minValue) {
+			return this.minValue;
 		}
 		return atn;
 	},
-	coefficientCo2: function(fuel_type, co2) {
+	co2Coefficient: function(fuel_type, co2) {
 		if(fuel_type === 'gazoil') {
-			return this.floorCo2Coefficient(95, co2);
+			return this.calculateCo2Coefficient(95, co2);
 		} else { // essence, lpg, natural gaz
-			return this.floorCo2Coefficient(116, co2);
+			return this.calculateCo2Coefficient(116, co2);
 		}
 	},
-	floorCo2Coefficient: function(baseline, co2) {
+	calculateCo2Coefficient: function(baseline, co2) {
 		// TODO: implement minCoef + degress coef
 		var minCoef = 0.055;
 		var maxCoef = 0.18;
